8662e965dbaf0c8b0dc33040e79cbe20402a200f
Abed Ali <mdabedali15@gmail.com>
2024-04-06 12:01:41 +0600

    some style change in header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   const [showFavModal, setShowFavModal] = useState(false);
 
   return (
-    <header className="w-full z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
+    <header className="w-full z-50 bg-gradient-to-b from-black/60 to-black/0 md:pb-10">
       <nav className="container flex flex-col md:flex-row items-center justify-between py-6">
         <Logo />
         <div className="flex items-center mt-4 gap-4 relative justify-between">
